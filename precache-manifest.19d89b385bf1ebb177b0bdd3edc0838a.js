05bdb42ea59e0a6ae60ed0cd275d37bbe67ec811
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-13 14:24:04 +0000

    Deploy xitman78/itera_travis_workshop to github.com/xitman78/itera_travis_workshop.git:gh-pages

---
+++ precache-manifest.19d89b385bf1ebb177b0bdd3edc0838a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e6d8608450f98510e16aa0458818b4a",
+    "url": "/itera_travis_workshop/index.html"
+  },
+  {
+    "revision": "01c96a2dbad11af6372e",
+    "url": "/itera_travis_workshop/static/css/main.fa44479f.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/itera_travis_workshop/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "01c96a2dbad11af6372e",
+    "url": "/itera_travis_workshop/static/js/main.68111639.chunk.js"
+  },
+  {
+    "revision": "43e395533eddf17cffb3",
+    "url": "/itera_travis_workshop/static/js/runtime~main.4f640686.js"
+  },
+  {
+    "revision": "45e242791b7752b745a7ae53f265acd4",
+    "url": "/itera_travis_workshop/static/media/TravisCI.45e24279.png"
+  }
+]);
